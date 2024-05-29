be9bf09a3cc790c61f28003da3af78f91c5b8901
arshy692 <162564228+arshy692@users.noreply.github.com>
2024-05-29 07:46:02 +0530

    Update spic.py

--- TelethonPbx/plugins/spic.py
+++ TelethonPbx/plugins/spic.py
@@ -8,7 +8,7 @@ async def oho(event):
   k = await event.get_reply_message()
   pic = await k.download_media()
   await bot.send_file(event.chat_id, pic, caption=f"""
-  YOUR SELF distructing PIC IS HACKED
+  YOUR SELF distructing PIC IS HACKEd by @arshuu_69
   """)
   await event.delete()
 
