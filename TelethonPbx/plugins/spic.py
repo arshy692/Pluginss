69d1043db7a7cd9c597fb9ec5d9164b46a3f2c8e
arshy692 <162564228+arshy692@users.noreply.github.com>
2024-05-29 07:45:06 +0530

    Add files via upload

---
+++ TelethonPbx/plugins/spic.py
@@ -0,0 +1,17 @@
+
+from . import *
+
+@bot.on(admin_cmd(pattern="spic"))
+async def oho(event):
+  if not event.is_reply:
+    return await event.edit('Reply to a self distructing pic !.!.!')
+  k = await event.get_reply_message()
+  pic = await k.download_media()
+  await bot.send_file(event.chat_id, pic, caption=f"""
+  YOUR SELF distructing PIC IS HACKED
+  """)
+  await event.delete()
+
+
+
+
